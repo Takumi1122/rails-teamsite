593cafe48d6599cf319db5c4c87d38033d9d7346
Takumi Izumi <takumi221b4869@gmail.com>
2019-06-18 18:12:43 +0000

    finish ch6

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,18 +8,36 @@ class MembersController < ApplicationController
   end
 
   def new
+    @member = Member.new(birthday: Date.new(1980, 1, 1))
   end
 
   def edit
+    @member = Member.find(params[:id])
   end
 
   def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員を登録しました。"
+    else
+      render "new"
+    end
   end
 
   def update
+    @member = Member.find(params[:id])
+    @member.assign_attributes(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :members, notice: "会員を削除しました"
   end
 
   def search
